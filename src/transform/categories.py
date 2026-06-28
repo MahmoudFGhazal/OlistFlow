647b85e18112b57e18174a0b7136512c34e6a574
Mahmoud Ghazal <mahmoud.f.ghazal@gmail.com>
2026-06-28 19:40:32 -0300

    feat: category_clean

--- src/transform/categories.py
+++ src/transform/categories.py
@@ -1,18 +1,66 @@
 import logging
 import pandas as pd
 
+from .helper import validate_columns, validate_required_values
+
+"""
+product_category_name* string
+product_category_name_english string
+
+sem duplicatas
+"""
+
+PRODUCT_CATEGORY_NAME = "product_category_name"
+PRODUCT_CATEGORY_NAME_ENGLISH = "product_category_name_english"
+
+COLUMNS = [
+    PRODUCT_CATEGORY_NAME,
+    PRODUCT_CATEGORY_NAME_ENGLISH
+]
+
+REQUIRED_COLUMNS = [
+    PRODUCT_CATEGORY_NAME,
+]
 
 logger = logging.getLogger("etl.transform.categories")
 
 def transform_categories(df: pd.DataFrame) -> pd.DataFrame:
     logger.info("Transformando categorias")
  
-    df = df.drop_duplicates(subset=["product_category_name"])
-    df = df.rename(columns={
-        "product_category_name": "category_name_pt",
-        "product_category_name_english": "category_name_en",
-    })
-    df = df.dropna(subset=["category_name_pt"])
- 
+    validate_columns(df, required_columns=COLUMNS, table_name="categories")
+
+    df = _clean_categories(df)
+
+    df = validate_required_values(df, required_columns=REQUIRED_COLUMNS, table_name="categories")
+
+    df = df.drop_duplicates(
+        subset=[
+            PRODUCT_CATEGORY_NAME        
+        ]
+    )
+
     logger.info(f"Categories transformada ({len(df)} registros)")
+    return df
+
+def _clean_categories(df: pd.DataFrame) -> pd.DataFrame:
+    logger.info("Limpando categories")
+
+    df = df.copy()
+
+    df[PRODUCT_CATEGORY_NAME] = (
+        df[PRODUCT_CATEGORY_NAME]
+        .astype("string")
+        .str.strip()
+        .str.replace(r"\s+", " ", regex=True)
+        .str.lower()
+    )
+
+    df[PRODUCT_CATEGORY_NAME_ENGLISH] = (
+        df[PRODUCT_CATEGORY_NAME_ENGLISH]
+        .astype("string")
+        .str.strip()
+        .str.replace(r"\s+", " ", regex=True)
+        .str.lower()
+    )
+
     return df
--- src/transform/helper.py
+++ src/transform/helper.py
@@ -20,3 +20,24 @@ def normalize_nulls(df: pd.DataFrame) -> pd.DataFrame:
         pd.NA,
     )
     
+
+#Limpar
+def validate_columns(df: pd.DataFrame, required_columns: list[str], table_name: str) -> None:
+    missing = set(required_columns) - set(df.columns)
+
+    if missing:
+        logger.error(f"{table_name} - Possui as seguintes colunas ausentes: {sorted(missing)}")
+        raise ValueError(
+            f"{table_name} - Possui as seguintes colunas ausentes: {sorted(missing)}"
+        )
+
+def validate_required_values(df: pd.DataFrame, required_columns: list[str], table_name: str) -> pd.DataFrame:
+    nulls = df[required_columns].isna().sum()
+
+    invalid = nulls[nulls > 0]
+
+    if not invalid.empty:
+        logger.info(f"{table_name} - Colunas com valores nulos:\n{invalid}")
+        logger.info(f"{table_name} - Total de valores nulos encontrados: {invalid.sum()}")
+
+    return df.dropna(subset=required_columns)
