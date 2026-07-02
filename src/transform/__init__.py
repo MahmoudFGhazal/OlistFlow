25ec071f42245fb41517940be5bdfb2a7cfa7247
Mahmoud Ghazal <mahmoud.f.ghazal@gmail.com>
2026-07-02 11:43:53 -0300

    feat: add_missing_transforms

--- src/transform/__init__.py
+++ src/transform/__init__.py
@@ -3,6 +3,8 @@ from typing import TypedDict
 
 import pandas as pd
 
+from .tables.review import transform_reviews
+from .tables.payment import transform_payments
 from .tables.order_item import transform_order_items
 from .helper import normalize_nulls
 from .tables.customer import transform_customers
@@ -40,8 +42,8 @@ def transform_datasets(raw: dict[str, pd.DataFrame]) -> TransformedDatasets:
     products = transform_products(raw["products"])
     orders = transform_orders(raw["orders"])
     order_items = transform_order_items(raw["order_items"])
-    ##payments = transform_payments(raw["payments"])
-    #reviews = transform_reviews(raw["reviews"])
+    payments = transform_payments(raw["payments"])
+    reviews = transform_reviews(raw["reviews"])
 
     logger.info("Transformação concluídas")
 
@@ -52,7 +54,7 @@ def transform_datasets(raw: dict[str, pd.DataFrame]) -> TransformedDatasets:
         sellers=sellers,
         products=products,
         orders=orders,
-        #order_items=order_items,
-        #payments=payments,
-        #reviews=reviews,
+        order_items=order_items,
+        payments=payments,
+        reviews=reviews,
     )
